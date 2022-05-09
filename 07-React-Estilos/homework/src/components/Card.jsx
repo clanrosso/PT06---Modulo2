c147150e79332b0a16b55f3e8cf799f8244069fc
clanrosso <claudiorossocba@hotmail.com.ar>
2022-05-09 14:23:20 -0300

    ready

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React from "react";
+import s from "./Card.module.css";
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
-};
+  return (
+    <div className={s.tarjeta}>
+      <button className={s.boton} onClick={props.onClose}>
+        X
+      </button>
+      <h4 className={s.titulo}>{props.name}</h4>
+
+      <div className={s.subContenedor}>
+        <div className={s.temperatura}>
+          <p>Max</p>
+          <p>{props.max}</p>
+        </div>
+
+        <div className={s.temperatura}>
+          <p>Min</p>
+          <p>{props.min}</p>
+        </div>
+        <img
+          className={s.imagen}
+          src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React from "react";
+import s from "./SearchBar.module.css";
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
-};
+  return (
+    <div className={s.contenedor}>
+      <input className={s.input} type={"text"} placeholder="Ciudad..."></input>
+      <button className={s.boton} onClick={() => props.onSearch()}>
+        Agregar
+      </button>
+    </div>
+  );
+}
