2f1ddfb01a282f146ba8a1df18895aaa60267fa4
clanrosso <claudiorossocba@hotmail.com.ar>
2022-05-13 11:14:29 -0300

    ready

---
+++ 09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <main className="about">
+      <h5>About Weather App</h5>
+      <h6>Soy Henry</h6>
+      <p>
+        Esta es una aplicacion del clima realizada en la homework del modulo dos
+        del cohorte PT-6
+      </p>
+    </main>
+  );
+}
--- 09-React-Routing/homework/src/components/Nav.jsx
+++ 09-React-Routing/homework/src/components/Nav.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import Logo from '../img/logoHenry.png'
-import SearchBar from './SearchBar.jsx';
-import './Nav.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../img/logoHenry.png";
+import SearchBar from "./SearchBar.jsx";
+//import About from "./About.jsx";
+import "./Nav.css";
 
-
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
   return (
     <nav className="navbar navbar-dark bg-dark">
+      <Link to="/">
         <span className="navbar-brand">
-          <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          <img
+            id="logoHenry"
+            src={Logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+          />
           Henry - Weather App
         </span>
-        <SearchBar
-          onSearch={onSearch}
-        />
+      </Link>
+
+      <SearchBar onSearch={onSearch} />
     </nav>
   );
-};
+}
 
 export default Nav;
